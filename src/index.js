c0fa84403c0b1c0adabbb38958c630f9ddd34f1f
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-11-13 01:27:57 +0530

    try8

--- src/index.js
+++ src/index.js
@@ -7,6 +7,44 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+const overFlow = {
+    status  : "error",
+    message : "Overflow"
+}
+
+const underFlow = {
+    status  :   "error",
+    message :   "Underflow" 
+}
+
+const invalid = {
+    status  :   "error",
+    message :   "invalid data types"
+}
+
+function validator(num1, num2) {
+    //check for invalid data type
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        res.status(400).send(invalid);
+        res.end();
+        return;
+    };
+
+    //check for value higher than 10M
+    if(num1 > 1000000 || num2 > 1000000){
+        res.status(400).send(overFlow);
+        res.end();
+        return;
+    };
+
+    //check for value lesser than -10M
+    if(num1 < -1000000 || num2 < -1000000){
+        res.status(400).send(underFlow);
+        res.end();
+        return;
+    }
+    return;
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -23,24 +61,8 @@ app.post("/add", (req, res) => {
     const num1 = req.body.num1;
     const num2 = req.body.num2;
 
-    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
-        res.status(400).send({
-            status : "error",
-            message : "invalid data types",
-        });
-        res.end();
-        return;
-    }
+    validator(num1, num2);
 
-    if(num1 >= 1000000 || num2 >= 1000000){
-        res.status(400).send({
-            status : "failure",
-            message : "Overflow"
-        });
-        res.end();
-        return;
-    }
-   
     res.status(200).send({
         status : "success",
         message : "the sum of given two numbers",
@@ -55,33 +77,8 @@ app.post("/sub", (req, res) => {
     
     const num1 = req.body.num1;
     const num2 = req.body.num2;
-   
-    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
-        res.status(200).send({
-            status : "error",
-            message : "invalid data types"
-        });
-        res.end();
-        return;
-    }
-
-    if(num1 < 1000000){
-        res.status(200).send({
-            status : "failure",
-            message : "Underflow"
-        });
-        res.end();
-        return;
-    }
-    if(num2 < 1000000){
-        res.status(200).send({
-            status : "failure",
-            message : "Underflow"
-        });
-        res.end();
-        return;
-    }
-   
+    
+    validator(num1, num2);
     res.status(200).send({
         status : "success",
         message : "the difference of given two numbers",
@@ -97,23 +94,7 @@ app.post("/multiply", (req, res) => {
     const num1 = req.body.num1;
     const num2 = req.body.num2;
    
-    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
-        res.status(200).send({
-            status : "error",
-            message : "invalid data types"
-        });
-        res.end();
-        return;
-    }
-
-    if(num1 > 1000000 || num2 > 1000000){
-        res.status(200).send({
-            status : "failure",
-            message : "Overflow"
-        });
-        res.end();
-        return;
-    }
+    validator(num1, num2);
    
     res.status(200).send({
         status : "success",
@@ -130,18 +111,11 @@ app.post("/division", (req, res) => {
     const num1 = req.body.num1;
     const num2 = req.body.num2;
    
-    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
-        res.status(200).send({
-            status : "error",
-            message : "invalid data types"
-        });
-        res.end();
-        return;
-    }
-
+    validator(num1, num2);
+    
     if(num2 ==0){
-        res.status(200).send({
-            status : "failure",
+        res.status(400).send({
+            status : "error",
             message : "Cannot divide by zero"
         });
         res.end();
